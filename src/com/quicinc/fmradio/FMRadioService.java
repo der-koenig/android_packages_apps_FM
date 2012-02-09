7c405f81b5c3d5d2648dd38c9ec12e798f81b138
Venkateshwarlu Domakonda <Venkateshwarlu@codeaurora.org>
2012-02-09 17:09:21 +0530

    FM-APP: Correct the implementation of FM and call concurrency.
    Maintain call status locally and use the same for next successive FM ON
    
    Change-Id: I0459f28c25c74b4b87c16fd2cc0583c3e614008a
    CRs-Fixed: 331456

--- src/com/quicinc/fmradio/FMRadioService.java
+++ src/com/quicinc/fmradio/FMRadioService.java
@@ -109,6 +109,7 @@ public class FMRadioService extends Service
    private boolean mFMOn = false;
    private boolean mFmRecordingOn = false;
    private boolean mSpeakerPhoneOn = false;
+   private int mCallStatus = 0;
    private static boolean mRadioState = true;
    private BroadcastReceiver mScreenOnOffReceiver = null;
    final Handler mHandler = new Handler();
@@ -897,6 +898,7 @@ public class FMRadioService extends Service
    //if Call Status is non IDLE we need to Mute FM as well stop recording if
    //any. Similarly once call is ended FM should be unmuted.
        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+       mCallStatus = state;
 
        if((TelephonyManager.CALL_STATE_OFFHOOK == state)||
           (TelephonyManager.CALL_STATE_RINGING == state)) {
@@ -908,6 +910,7 @@ public class FMRadioService extends Service
            }
        boolean bTempSpeaker = mSpeakerPhoneOn; //need to restore SpeakerPhone
        boolean bTempMute    = mMuted;// need to restore Mute status
+       int bTempCall = mCallStatus;//need to restore call status
        fmOff();
        try
            {
@@ -923,6 +926,7 @@ public class FMRadioService extends Service
             }
            mResumeAfterCall = true;
            mSpeakerPhoneOn = bTempSpeaker;
+           mCallStatus = bTempCall;
            mMuted = bTempMute;
        }
        else if (state == TelephonyManager.CALL_STATE_IDLE) {
@@ -2145,8 +2149,7 @@ public class FMRadioService extends Service
    }
    public int getCallState()
    {
-       TelephonyManager tmgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-       return tmgr.getCallState();
+       return mCallStatus;
    }
 
    /* Receiver callbacks back from the FM Stack */
