1fa0b29312ec989923469970f82dc800e6e4cbe1
Matt Koenig <der_koenig@rocketmail.com>
2014-03-12 20:19:36 +0100

    Fix platform dependencies

--- Android.mk
+++ Android.mk
@@ -10,7 +10,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src/com/quicinc/fmradio) \
 	src/com/quicinc/fmradio/IFMTransmitterService.aidl \
 
 
-ifeq ($(call is-android-codename-in-list,JELLY_BEAN ICECREAM_SANDWICH HONEYCOMB),true)
+ifeq ($(call is-platform-sdk-version-at-least,11),true)
 LOCAL_SRC_FILES +=  $(call all-java-files-under, src/com/quicinc/hc_utils)
 else
 LOCAL_SRC_FILES +=  $(call all-java-files-under, src/com/quicinc/utils)
